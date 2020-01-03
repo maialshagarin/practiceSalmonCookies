a7ba7d8fbe84c9aeba7f9e3380fa50718399c947
mai <mai.yusuf28@yahoo.com>
2020-01-03 21:03:20 +0200

    doing salmon cookies using jquery

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-// here i use DOM , let  and foreach to do salmon cookies which i take in 201 
+// here i use jQuery to do salmon cookies which i take in 201 
 
 
 "use strict";
 // // global  variable 
-let footer;
+// let footer;
 let hours = ["6 am ", "7 am ", "8 am ", "9 am ", "10 am ", "11 am ", "12 am ", "1 pm ", "2 pm ", "3 pm ", "4 pm ", "5 pm ", "6 pm ", "7 pm "];
 
 
@@ -52,155 +52,118 @@ let shopsList = [seattle, tokyo, paris, lima, dubai]
 
 
 // ///////////////////////////////////////////////////////////////////
-// render on table use dom 
-let container = document.getElementById('content-area');
-//// create table 
-let table = document.createElement('table');
-container.appendChild(table);
-/// create the header raw 
-function headerRow() {
-    let headerRow = document.createElement('tr');
-    table.appendChild(headerRow);
+// render on table use jquery  
+$('#content-area').html('<table></table>');
+let row = $(`<tr></tr>`);
+let headerRowCell = $(`<th></th>`);
+let cell = $(`<td></td>`);
 
-    let th = document.createElement('th');
-    headerRow.appendChild(th);
-    th.textContent = "  ";
+
+
+
+
+
+////////// header row /////////////
+
+function headerRow() {
+    //     let headerRow = document.createElement('tr');
+    //     table.appendChild(headerRow);
+    $('table').append(row.append(headerRowCell.text('xxxxx')));
 
     hours.forEach(element => {
-        let th = document.createElement('th');
-        headerRow.appendChild(th);
-        th.textContent = element;
-
+        // console.log("element", element)
+        $('tr').append(`<th>${element}</th>`)
     });
-    let otherTh = document.createElement('th');
-    headerRow.appendChild(otherTh);
-    otherTh.textContent = "  Daily Total";
+    $('tr').append(`<th> Daily total <th>`);
 }
 
-/// put the hourly sales inside the table to each salmon cookies shops using two foreach and DOM 
+
+
+
+
+///// cell information for table ( table body) //////
 
 function dataInCell() {
+    // to show the name of location 
     shopsList.forEach(element => {
-        let shoprow = document.createElement('tr');
-        table.appendChild(shoprow);
-        let shopCell = document.createElement('Td');
-        shoprow.appendChild(shopCell);
-        shopCell.textContent = element.location;
-
-
-
+        $('table').append($(`<tr class ="${element.location}" ><td> ${element.location}</td></tr>`));
+        // to add the daily sale for each hour for each shop 
         hours.forEach(hour => {
 
-            let cellInput = document.createElement('td');
-            shoprow.appendChild(cellInput);
-            cellInput.textContent = element.hourlySales[hour.length];
+            $(`.${element.location}`).append($(`<td>${element.hourlySales[hour.length]}</td>`))
         });
-
-        let shopLastTd = document.createElement('td');
-        shoprow.appendChild(shopLastTd);
-
-        shopLastTd.textContent = element.dailySales;
+        $(`.${element.location}`).append($(`<td>${element.dailySales}</td>`))
     });
-
-};
+}
 
 
 
 //  DO the footer raw which has the total for each hour and the hole total 
 function footerRawData() {
-    let footerRowTr = document.createElement('tr');
-    footer = footerRowTr;
-    table.appendChild(footerRowTr);
-    let total = document.createElement('td');
-    footerRowTr.appendChild(total);
-    total.textContent = 'Totals';
-
+    $('table').append($(`<tr class ="TheFooter" > <td>Totals</td></tr>)`));
     let megaTotal = 0;
 
     hours.forEach(element => {
-        let td = document.createElement('td');
-        footer.appendChild(td);
-
         let sum = 0;
         shopsList.forEach(shop => {
             sum += shop.hourlySales[element.length];
         });
-
-        td.textContent = sum;
+        $(`.TheFooter`).append(`<td>${sum} </td>`)
 
         megaTotal += sum;
     });
-    let td = document.createElement('td');
 
-    footer.appendChild(td);
-
-    td.textContent = megaTotal;
+    $(`.TheFooter`).append(`<td>${megaTotal}</td>`)
 
 }
+
+
+
+
+// //////////// this function create the raw which will hold the information for new shop that came from the form in the below function 
+function renderRaw() {
+    let thecurrent = shopsList[shopsList.length - 1];
+    $('table').append($(`<tr class ="${thecurrent.location}" ><td> ${thecurrent.location}</td></tr>`));
+   
+    hours.forEach(element => {
+
+        $(`.${thecurrent.location}`).append(`<td>${thecurrent.hourlySales[element.length]} </td>`)
+
+    });
+
+    $(`.${thecurrent.location}`).append($(`<td>${thecurrent.dailySales}</td>`))
+
+
+}
+
+
 
 
 // form 
-////////////// make form , allow user to acsses that  and add the new one to the table ///
-let form = document.getElementById('addNewShop');
-
-
-//////////// this function create the raw which will hold the information for new shop 
-function renderRaw() {
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-    let td = document.createElement('td');
-    tr.appendChild(td);
-    let thecurrent = shopsList[shopsList.length - 1];
-    console.log('thrrrrrrrrrrr', thecurrent);
-    td.textContent = thecurrent.location;
-
-    hours.forEach(element => {
-
-        let info = document.createElement('td');
-        tr.appendChild(info);
-        info.textContent = thecurrent.hourlySales[element.length];
-    });
-    let newRawTd = document.createElement('td');
-    tr.appendChild(newRawTd);
-    newRawTd.textContent = thecurrent.dailySales;
-}
-
-
-/////////////// this function listen to the form we add remove the raw  of total add the new raw for the new shop via call the render then re add the total raw  
-function afterSubmmit(event) {
-    event.preventDefault();
+// ////////////// make form , allow user to acsses that  and add the new one to the table ///
+$('#addNewShop').submit(function (afterSubmmit) {
+    afterSubmmit.preventDefault();
 
     let locationName = event.target.location.value;
     let min = parseInt(event.target.min.value);
     let max = parseInt(event.target.max.value);
     let avgSales = parseFloat(event.target.average.value);
 
-
-///// to control what exactly but inside the field form 
-    // if (!event.target.name.value || !event.target.minCustomers.value || !event.target.maxCustomers.value || !event.target.avgSale.value) {
-    //     return alert('Oops, you forgot something! Please check your entries and try again.');
-    //   }
-    
-    //   if(event.target.minCustomers.value < 0 || event.target.maxCustomers.value < 0 || event.target.avgSale.value < 0) {
-    //     return alert ('How are you putting negative values in here? Quit it, you\re not giving away customers and cookies.');
-    //   }
-    
-    //   if (event.target.minCustomers.value > event.target.maxCustomers.value) {
-    //     return alert('That\'s not how numbers work, nerd.');
-    //   }
     let newShop = new SweetShops(locationName, min, max, avgSales);
     shopsList.push(newShop);
-    // newShop.renderRaw(table);
-    table.removeChild(footer);
+//////remove the footer row then render the new one after that return the footer row 
+    $(`.TheFooter`).remove();
 
-    renderRaw(table);
-    footerRawData(table);
-}
+    renderRaw(shopsList[-1]);
+    footerRawData();
+
+})
 
 
-////////calling the function ///////////
-headerRow(table);
-dataInCell(table);
-footerRawData(table);
-form.addEventListener('submit', afterSubmmit);
-////////////////////////////////////////
+
+
+// ////////calling the function ///////////
+headerRow();
+dataInCell();
+footerRawData();
+// ////////////////////////////////////////
